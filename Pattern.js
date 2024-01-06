b8db1ae602a32d99fa32d2324a5f5fa89ae7e55d
Raj Kumar Gupta <118257845+jmdrajgupta@users.noreply.github.com>
2024-01-06 14:38:06 +0530

    Update Pattern.js

--- Pattern.js
+++ Pattern.js
@@ -1,2 +1,27 @@
+# Solid Rectangle
+*****
+*****
+*****
+*****
+*****
+  
+<script>
+  
+const rows= Number(window.prompt("No of Rows??", ""));
+const columns= Number(window.prompt("No of Columns ??", ""));
 
-// Triangle 
+
+      for(let i =0;i<rows;i++){
+        document.write("*")
+      
+
+    
+     
+          for(let j=1;j<columns;j++){
+            document.write("*")
+          }
+         document.write("<br/>")
+         
+        }
+      
+  </script>

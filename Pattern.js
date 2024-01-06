ef9a3273cc987200e2b85f94849ea39abffd44c4
Raj Kumar Gupta <118257845+jmdrajgupta@users.noreply.github.com>
2024-01-06 14:40:20 +0530

    Update Pattern.js

--- Pattern.js
+++ Pattern.js
@@ -9,19 +9,11 @@
   
 const rows= Number(window.prompt("No of Rows??", ""));
 const columns= Number(window.prompt("No of Columns ??", ""));
-
-
-      for(let i =0;i<rows;i++){
-        document.write("*")
-      
-
-    
-     
-          for(let j=1;j<columns;j++){
-            document.write("*")
-          }
-         document.write("<br/>")
-         
-        }
+   for(let i =0;i<rows;i++){
+   document.write("*")
+      for(let j=1;j<columns;j++){
+            document.write("*");}
+            document.write("<br/>")
+         }
       
   </script>

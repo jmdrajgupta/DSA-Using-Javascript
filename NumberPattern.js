cc413929ec7c03447a14e9998a59bcad015c9bfb
Raj Kumar Gupta <118257845+jmdrajgupta@users.noreply.github.com>
2024-01-14 13:18:58 +0530

    column NumberPattern.js

--- NumberPattern.js
+++ NumberPattern.js
@@ -12,3 +12,19 @@
       document.write("<br>")
     }
   </script>
+
+0
+01
+012
+0123
+01234
+
+  <script>
+    for (let i=0;i<5;i++){
+      for (let j=0;j<=i;j++){
+      document.write(j)
+    }
+      document.write("<br>")
+    }
+  </script>
+     
